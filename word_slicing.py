42fee033b0a267eecec649403defc5ba824a6683
K3YMONEY <wairimuiruri21@gmail.com>
2025-04-25 10:53:59 +0300

    word_slicing.py

---
+++ word_slicing.py
@@ -0,0 +1,9 @@
+word = "Holbertson"
+word_first_3= word[0:3]
+word_last_2= word[8:]
+middle_word= word[1:9]
+last_letter= word[9:]
+
+print(f"first 3 letters: {word_first_3}")
+print(f"last 2 letters: {word_last_2}")
+print(f"middle word: {middle_word}")
