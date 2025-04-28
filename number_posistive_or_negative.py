35ac9b9a304d13c939a57812cc97203e27d58ee9
K3YMONEY <wairimuiruri21@gmail.com>
2025-04-28 16:29:14 +0300

    number_posistive_or_negative.py

---
+++ number_posistive_or_negative.py
@@ -0,0 +1,13 @@
+''' Check if the number is positive or negative '''
+number = input("Enter number " )
+number = int(number)
+
+if number > 0 :
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+
+
+
