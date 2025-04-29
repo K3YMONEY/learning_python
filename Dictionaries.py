058c6d9dc9befcb0859ebccca1b0e7918e35b816
K3YMONEY <wairimuiruri21@gmail.com>
2025-04-29 21:03:24 +0300

    Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,7 @@
+info_person = {
+             'Name' : 'Robert',
+             'School' : 'IMTR',
+             'Age' : 32,
+             'Course' : ['arts', 'met']
+              }
+print(info_person)
