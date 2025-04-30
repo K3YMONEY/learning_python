cf27261b4fc2ac6729c8a7630d8cc8a7b4e5ed5a
K3YMONEY <wairimuiruri21@gmail.com>
2025-04-30 08:57:36 +0300

    FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+def FizzBuzz():
+    for k in range(1, 101):
+        if k % 3 == 0 and k % 5 == 0:
+            print('FizzBuzz', end='')
+        elif k % 3 == 0:
+            print('Fizz', end='')
+        elif k % 5 == 0:
+            print('Buzz', end='')
+        else:
+            print(k, end='')
+print(FizzBuzz())
+
