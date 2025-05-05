6314e29f0ac98799bf24479b91ddf8616bb1b6d1
K3YMONEY <wairimuiruri21@gmail.com>
2025-05-05 17:33:30 +0300

    2_dictionary.py

---
+++ 2_dictionary.py
@@ -0,0 +1,15 @@
+
+''' write a python program that takes 3 inputs from the user name, age and city. store it in  dict and print the dict'''
+name = input('Enter name: ')
+age = input('Enter age: ')
+city = input('Enter city: ')
+
+user_info = {
+        'name' : name,
+        'age' : age, 
+        'city' : city
+}
+
+print(user_info)
+
+
