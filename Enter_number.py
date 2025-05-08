9cc75181af7f657d8ac354b243a13bd0c774e506
K3YMONEY <wairimuiruri21@gmail.com>
2025-05-08 17:14:55 +0300

    Enter_number.py

---
+++ Enter_number.py
@@ -0,0 +1,14 @@
+num = input("Enter Name: ")
+print(f" Hello, {num}")
+a = input("Enter first number: ")
+a = int(a)
+b = input("Enter second number: ")
+b = int(b)
+
+print(a + b) # find the sum
+print(a - b) # find the difference
+print(a * b) # find the product
+print(a / b) # find quotient
+
+print(f"Thanks, {num}! Your numbers have been processed.")
+
